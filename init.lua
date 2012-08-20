2dae98c1b7ace8ca6f788820a115e518211f08f9
Bas <bassimhuis@gmail.com>
2012-08-21 01:36:37 +0200

    Added seperate pieces and rules lua files and inserted pawn node to pieces lua

--- init.lua
+++ init.lua
@@ -12,6 +12,9 @@
 -- [regis] board_white
 -- ======================================
 
+dofile(minetest.get_modpath("chess").."/pieces.lua")
+dofile(minetest.get_modpath("chess").."/rules.lua")
+
 -- Register the spawn block
 minetest.register_node(":chess:spawn",{
     description = "Chess Board",
@@ -73,37 +76,4 @@ minetest.register_node(":chess:board_black",{
     tile_images = {"chess_black.png"},
     inventory_image = "chess_black.png",
     groups = {tree=1,snappy=1,choppy=2,oddly_breakable_by_hand=1,flammable=2},
-})
-
-
---This file declares all the pieces for the game
-
---Declare the pawns
-minetest.register_node(":chess:pawn",
-{
-    description = 'pawn',
-    groups = {snappy = 2},
-    tiles = {"chess_black.png"},
-    drawtype = "nodebox",
-    sunlight_propagates = true,
-    paramtype = 'light',
-    paramtype2 = "facedir",
-    light_source = 8, --max light is 18
-    node_box = {
-    type = "fixed",
-    fixed = {
-    {-0.2, -0.5, -0.3, 0.2, -0.4, 0.3},
-    {-0.3, -0.5, -0.2, 0.2, -0.4, 0.3},
-    {-0.1, -0.4, -0.2, 0.1, -0.3, 0.2},
-    {-0.2, -0.4, -0.1, 0.2, -0.3, 0.1},
-    {-0.1, -0.3, -0.1, 0.1, 0.2, 0.1},
-    {-0.2, -0.1, -0.1, 0.2, 0.1, 0.1},
-    {-0.1, -0.1, -0.2, 0.1, 0.1, 0.2},
-    },
-    },
-    selection_box = {
-    type = "fixed",
-    fixed = {-0.3, -0.5, -0.3, 0.3, 0.2, 0.3},
-    },
-    groups = {snappy=2,choppy=2,oddly_breakable_by_hand=2}
 })
