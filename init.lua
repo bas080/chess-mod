5df3b47fcaac874d30deebcc1b925d1dff65db3f
Bas <bassimhuis@gmail.com>
2012-08-21 02:24:46 +0200

    Added new images for pawn textures and added loop to make black and white version of pieces. Also fixed the board creation. Not sure if working correctly though

--- init.lua
+++ init.lua
@@ -23,11 +23,11 @@ minetest.register_node(":chess:spawn",{
 	groups = {tree=1,snappy=1,choppy=2,oddly_breakable_by_hand=1,flammable=2},
     after_place_node = function(pos)
         
-        local size = 20
+        local size = 8
         local alternate = true
         
-        for i = size, 0, -1 do
-            for ii = size, 0, -1 do
+        for i = size, 1, -1 do
+            for ii = size, 1, -1 do
             
                 local p = {x=pos.x+i+1, y=pos.y, z=pos.z+ii+1}
                 
@@ -40,7 +40,7 @@ minetest.register_node(":chess:spawn",{
                 end
             end
             
-            if (math.floor(size/2) ~= size/2) then
+            if (math.floor(size/2) == size/2) then
                 if alternate then
                     alternate = false
                 else
@@ -65,7 +65,7 @@ minetest.register_craft({
 --Register the Board Blocks: white
 minetest.register_node(":chess:board_white",{
     description = "White Chess Board Piece",
-    tile_images = {"chess_white.png"},
+    tile_images = {"chess_board_white.png"},
     inventory_image = "chess_white.png",
     groups = {tree=1,snappy=1,choppy=2,oddly_breakable_by_hand=1,flammable=2},
 })
@@ -73,7 +73,7 @@ minetest.register_node(":chess:board_white",{
 --Register the Board Blocks: black
 minetest.register_node(":chess:board_black",{
     description = "Black Chess Board Piece",
-    tile_images = {"chess_black.png"},
+    tile_images = {"chess_board_black.png"},
     inventory_image = "chess_black.png",
     groups = {tree=1,snappy=1,choppy=2,oddly_breakable_by_hand=1,flammable=2},
 })
