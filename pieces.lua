e12f89550ef6485ca1e05d4dc4c06eee97789286
tony-ka <antonio.arjona.melches@gmail.com>
2012-08-25 15:07:43 +0200

    add rook piece

--- pieces.lua
+++ pieces.lua
@@ -45,6 +45,43 @@ for color = 1, 2 do
     })
 
     --Rook
+    minetest.register_node("chess:rook_"..colors[color],
+    {
+        description = 'rook',
+        groups = {snappy = 2},
+        tiles = {"chess_piece_"..colors[color]..".png"},
+        drawtype = "nodebox",
+        sunlight_propagates = true,
+        paramtype = 'light',
+        paramtype2 = "facedir",
+        light_source = 8, --max light is 18
+        node_box = {
+        type = "fixed",
+        fixed = {
+		{-0.2, -0.5, -0.3, 0.2, -0.4, 0.3},
+		{-0.3, -0.5, -0.2, 0.3, -0.4, 0.2},
+		{-0.1, -0.4, -0.2, 0.1, -0.3, 0.2},
+		{-0.2, -0.4, -0.1, 0.2, -0.3, 0.1},
+		{-0.1, -0.3, -0.1, 0.1, 0.2, 0.1},
+		{-0.1, 0.2, -0.2, 0.1, 0.3, 0.2 },
+		{-0.2, 0.2, -0.1, 0.2, 0.3, 0.1},
+		{-0.2, 0.3, -0.2, 0.2, 0.4, 0.2},
+		{-0.2, 0.4, -0.2, -0.1, 0.5, -0.1},
+		{-0.05, 0.4, -0.2, 0.05, 0.5, -0.1},
+		{0.1, 0.4, -0.2, 0.2, 0.5, -0.1},
+		{-0.2, 0.4, -0.05, -0.1, 0.5, 0.05},
+		{0.1, 0.4, -0.05, 0.2, 0.5, 0.05},
+		{-0.2, 0.4, 0.1, -0.1, 0.5, 0.2},
+		{-0.05, 0.4, 0.1, 0.05, 0.5, 0.2},
+		{0.1, 0.4, 0.1, 0.2, 0.5, 0.2},
+        },
+		},
+		selection_box = {
+        type = "fixed",
+        fixed = {-0.3, -0.5, -0.3, 0.3, 0.5, 0.3},
+		},
+        groups = {snappy=2,choppy=2,oddly_breakable_by_hand=2}
+    })
 
     --Knight
 
