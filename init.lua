742c736b75577ced1144da8362ba82ae9657655a
Bas <bassimhuis@gmail.com>
2012-09-14 03:51:42 +0200

    Improved the chessboard spawning and made the spawn block part of the board. Now also has nicer border nodes and letters and numbers on the side of the border

--- init.lua
+++ init.lua
@@ -15,22 +15,27 @@
 dofile(minetest.get_modpath("chess").."/pieces.lua")
 dofile(minetest.get_modpath("chess").."/rules.lua")
 
+local letters = {"a", "b", "c", "d", "e", "f", "g", "h"}
+
+local size = 9 --total width(10) including the border coordinate 0,0
+local innerSize = 8 --inner width(8) including the coordinate 1,1
+
 -- Register the spawn block
 minetest.register_node("chess:spawn",{
     description = "Chess Board",
     tile_images = {"chess_spawn.png"},
     inventory_image = "chess_spawn.png",
-	groups = {tree=1,snappy=1,choppy=2,oddly_breakable_by_hand=1,flammable=2},
+	  groups = {tree=1,snappy=1,choppy=2,oddly_breakable_by_hand=1,flammable=2},
     after_dig_node = function(pos, node, digger)
         
-        local size = 10
+        local size = 9
         
         local p = {x=pos.x+1, y=pos.y, z=pos.z+1}
         local n = minetest.env:get_node(p)
         
         if n.name == "chess:board_black" then 
-            for i = size, 1, -1 do
-                for ii = size, 1, -1 do
+            for i = size, 0, -1 do
+                for ii = size, 0, -1 do
                     
                     --remove board
                     local p = {x=pos.x+i, y=pos.y, z=pos.z+ii}
@@ -50,22 +55,12 @@ minetest.register_node("chess:spawn",{
         
     end,
     after_place_node = function(pos, placer)
-    	
-    	--spawn box ownership
-    	
-    	local player = placer:get_player_name()
-    	print(player)
-    	local placed = minetest.env:get_node(pos)
-    
-        local placed_meta = minetest.env:get_meta(placed)
-        placed_meta:set_string("owner", player)
+        --place chess board
         
-        --chessboard placement that is coded really ugly. I hate it!!! and i will fix it!!! I MUST!
-        local size = 10
         local isFree = true
         
-        for i = size, 1, -1 do --check if there is room for a chessboard with pieces ontop
-            for ii = size, 1, -1 do
+        for i = size, 0, -1 do --check if there is room for a chessboard with pieces ontop
+            for ii = size, 0, -1 do
                 
                 local p = {x=pos.x+i, y=pos.y, z=pos.z+ii}
                 local n = minetest.env:get_node(p)
@@ -73,78 +68,75 @@ minetest.register_node("chess:spawn",{
                 local p_top = {x=pos.x+i, y=pos.y+1, z=pos.z+ii}
                 local n_top = minetest.env:get_node(p_top)
                 
-                if n.name ~= "air" and n_top.name ~= "air" then 
+                if n.name ~= "air" and n_top.name ~= "air" then
                     isFree = false
+                    minetest.chat_send_all("Chess board does not fit")
                     break
                 end
                 
             end
         end
         
-        local alternate = true
-        
-        if isFree then -- if there is room for a chessboard and pieces, make the chessboard with pieces
+        if (isFree) then
             minetest.chat_send_all("Chess board has been placed, let the match begin!")
-            for i = size, 1, -1 do
-                for ii = size, 1, -1 do
-                    --place pieces
+            
+            for i = size, 0, -1 do
+                for ii = size, 0, -1 do
+                    --place chessboard
                     local p = {x=pos.x+i, y=pos.y, z=pos.z+ii}
                     local p_top = {x=pos.x+i, y=pos.y+1, z=pos.z+ii}
-
-                    if (i > 1 and i < size) then
-                        --black pieces
-                        if (ii == 3) then --pawns
-                            minetest.env:add_node(p_top, {name="chess:pawn_black"})
+                    
+                    if (ii == 0) or (ii == size) or (i ==0) or (i == size) then --if border
+                        
+                        if (ii == 0) and (i < size) and (i > 0) then --letters
+                            minetest.env:add_node(p, {name="chess:border_" .. letters[i]})
                         end
                         
-                        if (ii == 2) then --behind pawns
-                            if (i == 2 or i == 9) then minetest.env:add_node(p_top, {name="chess:rook_black"}) end
-                            if (i == 3 or i == 8) then minetest.env:add_node(p_top, {name="chess:knight_black"}) end
-                            if (i == 4 or i == 7) then minetest.env:add_node(p_top, {name="chess:bishop_black"}) end
-                            if (i == 6) then minetest.env:add_node(p_top, {name="chess:queen_black"}) end
-                            if (i == 5) then minetest.env:add_node(p_top, {name="chess:king_black"}) end
-                        end
-
-                        --white pieces
-                        if (ii == 8) then --pawns
-                            minetest.env:add_node(p_top, {name="chess:pawn_white"})
+                        if (i == 0) and (ii < size) and (ii > 0) then --letters
+                            minetest.env:add_node(p, {name="chess:border_" .. ii})
                         end
                         
-                        if (ii == 9) then --behind pawns
-                            if (i == 2 or i == 9) then minetest.env:add_node(p_top, {name="chess:rook_white"}) end
-                            if (i == 3 or i == 8) then minetest.env:add_node(p_top, {name="chess:knight_white"}) end
-                            if (i == 4 or i == 7) then minetest.env:add_node(p_top, {name="chess:bishop_white"}) end
-                            if (i == 5) then minetest.env:add_node(p_top, {name="chess:queen_white"}) end
-                            if (i == 6) then minetest.env:add_node(p_top, {name="chess:king_white"}) end
+                        if (i == size) or (ii == size) then
+                            minetest.env:add_node(p, {name="chess:border"})
                         end
                         
-                    end
-
-                    if (ii == 1) or (ii == size) or (i ==1) or (i == size) then--create border
-                        minetest.env:add_node(p, {name="chess:board_black"})
-                    else
-                        if alternate then
+                        
+                    else--if inside border
+                        if (((ii+i)/2) == math.floor((ii+i)/2)) then
                             minetest.env:add_node(p, {name="chess:board_black"})
-                            alternate = false
                         else
                             minetest.env:add_node(p, {name="chess:board_white"})
-                            alternate = true
                         end
                     end
-                end
-                
-                if (math.floor(size/2) == size/2) then
-                    if alternate then
-                        alternate = false
-                    else
-                        alternate = true
+                    --place peaces
+                    if (ii == 2) and (i>0) and (i<size) then --pawns
+                        minetest.env:add_node(p_top, {name="chess:pawn_black"})
+                    end
+                    
+                    if (ii == 1) then --behind pawns
+                        if (i == 1 or i == 8) then minetest.env:add_node(p_top, {name="chess:rook_black"}) end
+                        if (i == 2 or i == 7) then minetest.env:add_node(p_top, {name="chess:knight_black"}) end
+                        if (i == 3 or i == 6) then minetest.env:add_node(p_top, {name="chess:bishop_black"}) end
+                        if (i == 4) then minetest.env:add_node(p_top, {name="chess:queen_black"}) end
+                        if (i == 5) then minetest.env:add_node(p_top, {name="chess:king_black"}) end
+                    end
+
+                    --white pieces
+                    if (ii == 7) and (i>0) and (i<size) then --pawns
+                        minetest.env:add_node(p_top, {name="chess:pawn_white"})
+                    end
+                    
+                    if (ii == 8) then --behind pawns
+                        if (i == 1 or i == 8) then minetest.env:add_node(p_top, {name="chess:rook_white"}) end
+                        if (i == 2 or i == 7) then minetest.env:add_node(p_top, {name="chess:knight_white"}) end
+                        if (i == 3 or i == 6) then minetest.env:add_node(p_top, {name="chess:bishop_white"}) end
+                        if (i == 4) then minetest.env:add_node(p_top, {name="chess:queen_white"}) end
+                        if (i == 5) then minetest.env:add_node(p_top, {name="chess:king_white"}) end
                     end
                 end
             end
-        else
-            minetest.chat_send_all("Chess board does not fit")
         end
-	end,
+    end,
 })
 
 -- Add crafting for the spawn block
@@ -172,3 +164,28 @@ minetest.register_node("chess:board_black",{
     inventory_image = "chess_black.png",
     groups = {indestructable},
 })
+
+minetest.register_node("chess:border",{
+    description = "Black Chess Board Piece",
+    tile_images = {"chess_board_black.png", "chess_board_black.png", "chess_board_black.png^chess_border_side.png"},
+    inventory_image = "chess_black.png",
+    groups = {indestructable},
+})
+
+for iii = innerSize, 1, -1 do
+    print("chess_border_" .. letters[iii] .. ".png")
+    minetest.register_node("chess:border_" .. letters[iii],{
+        description = "White Chess Board Piece",
+        tile_images = {"chess_board_black.png^chess_border_" .. letters[iii] .. ".png", "chess_board_black.png", "chess_board_black.png^chess_border_side.png"},
+        inventory_image = "chess_white.png",
+        groups = {indestructable},
+    })
+    
+    print("chess_border_" .. iii .. ".png")
+    minetest.register_node("chess:border_" .. iii,{
+        description = "White Chess Board Piece",
+        tile_images = {"chess_board_black.png^chess_border_" .. iii .. ".png", "chess_board_black.png", "chess_board_black.png^chess_border_side.png"},
+        inventory_image = "chess_white.png",
+        groups = {indestructable},
+    })
+end
